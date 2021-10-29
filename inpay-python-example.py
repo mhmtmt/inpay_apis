232cf4c17b18b35f48468b1063ec6905d9fef07e
omrozh <53612475+omrozh@users.noreply.github.com>
2021-10-29 20:13:34 +0300

    Update inpay-python-example.py

--- inpay-python-example.py
+++ inpay-python-example.py
@@ -1,11 +1,17 @@
-import inpay
+from inpay import Charge
 
-charge = inpay.Charge("i8189010944132646309108259471370", "LdgcAeeru6A4HooHTyHbTds19AkJBvNnoK")
+customer_account_number = "account number"
+receiver_api = "receiver wallet api number"
+
+charge = Charge(customer_account_number, receiver_api)
 
 amount_of_a_single_installment = 250.0
 number_of_installments = 4
 # Can be up to 6
 # amount_of_a_single_installment * number_of_installments cannot exceed 3000
+
+second_receiver = "Wallet API No"
+commission_percentage = 10
 
 charge_amount = 500.0
 # No limit
@@ -15,3 +21,9 @@ charge.installmentCharge(float(amount_of_a_single_installment), int(number_of_in
 
 charge.charge(float(charge_amount))
 # To charge in a single payment
+
+charge.marketplaceCharge(charge_amount, second_receiver, commission_percentage)
+# To charge in a single payment, get marketplace commission and send directly to seller account
+
+charge.marketplaceInstallmentCharge(float(amount_of_a_single_installment), int(number_of_installments), second_receiver, commission_percentage)
+# To charge in installments, get marketplace commission fee and send directly to seller account
